1796780701d7d1ea44fd86bf9e46d5197abca59d
Fernando Berti <bertilxi@gmail.com>
2022-08-27 11:07:11 -0300

    fix imports

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,34 +1,34 @@
-import type { GetServerSideProps } from 'next'
-import type { SiteMap } from 'lib/types'
-import { host } from 'lib/config'
-import { getSiteMap } from 'lib/get-site-map'
+import type { GetServerSideProps } from "next";
+import type { SiteMap } from "../lib/types";
+import { host } from "../lib/config";
+import { getSiteMap } from "../lib/get-site-map";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  if (req.method !== 'GET') {
-    res.statusCode = 405
-    res.setHeader('Content-Type', 'application/json')
-    res.write(JSON.stringify({ error: 'method not allowed' }))
-    res.end()
+  if (req.method !== "GET") {
+    res.statusCode = 405;
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify({ error: "method not allowed" }));
+    res.end();
     return {
-      props: {}
-    }
+      props: {},
+    };
   }
 
-  const siteMap = await getSiteMap()
+  const siteMap = await getSiteMap();
 
   // cache for up to 8 hours
   res.setHeader(
-    'Cache-Control',
-    'public, max-age=28800, stale-while-revalidate=28800'
-  )
-  res.setHeader('Content-Type', 'text/xml')
-  res.write(createSitemap(siteMap))
-  res.end()
+    "Cache-Control",
+    "public, max-age=28800, stale-while-revalidate=28800"
+  );
+  res.setHeader("Content-Type", "text/xml");
+  res.write(createSitemap(siteMap));
+  res.end();
 
   return {
-    props: {}
-  }
-}
+    props: {},
+  };
+};
 
 const createSitemap = (siteMap: SiteMap) =>
   `<?xml version="1.0" encoding="UTF-8"?>
@@ -49,8 +49,8 @@ const createSitemap = (siteMap: SiteMap) =>
           </url>
         `.trim()
       )
-      .join('')}
+      .join("")}
   </urlset>
-`
+`;
 
-export default () => null
+export default () => null;
